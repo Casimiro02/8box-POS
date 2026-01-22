fc652a3fec2ecd9208e4fb92f287eff98dc5b1eb
Galobiano <151698267+Galobiano@users.noreply.github.com>
2026-01-22 14:40:39 +0800

    feat: added constant navigation

---
+++ src/constants/nav.ts
@@ -0,0 +1,45 @@
+import { FaHouseChimney } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaHistory } from "react-icons/fa";
+import { FaChartPie } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import type { IconType } from "react-icons";
+
+interface NavItem {
+  label: string;
+  href: string;
+  icon: IconType;
+}
+
+export const navItems: NavItem[] = [
+  {
+    label: "Home",
+    href: "/",
+    icon: FaHouseChimney,
+  },
+  {
+    label: "Orders",
+    href: "/orders",
+    icon: FaShoppingCart,
+  },
+  {
+    label: "History",
+    href: "/history",
+    icon: FaHistory,
+  },
+  {
+    label: "Clock-in",
+    href: "/clock-in",
+    icon: FaHistory,
+  },
+  {
+    label: "Report",
+    href: "/report",
+    icon: FaChartPie,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: IoMdSettings,
+  },
+];
